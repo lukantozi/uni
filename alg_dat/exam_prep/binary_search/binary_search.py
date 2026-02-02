6a359b90a2ef14566dbbe2dfe0f8ff9513b63823
lukantozi <lukamamri@outlook.com>
2026-02-02 16:30:13 +0100

    fix errors

--- alg_dat/exam_prep/binary_search/binary_search.py
+++ alg_dat/exam_prep/binary_search/binary_search.py
@@ -17,52 +17,64 @@ def binary_search_first(arr, target, low=0, high=None):
     if high is None:
         high = len(arr) - 1
 
+    result = -1
     while high >= low:
         mid = low + (high - low) // 2
-        
-        if arr[mid] == target:
-            while arr[mid] == target:
-                mid -= 1
-            return mid + 1
 
+        if arr[mid] == target:
+            result = mid
+            high = mid - 1
         elif arr[mid] > target:
             high = mid - 1
         else:
             low = mid + 1
-    return -1
+    return result
+
 #print(binary_search_first([1, 3, 3, 4, 12, 12, 12, 14], 3))
+#print(binary_search_first([3,3,3],3))
 
 def binary_search_last(arr, target, low=0, high=None):
     if high is None:
         high = len(arr) - 1
 
+    result = -1
     while high >= low:
         mid = low + (high - low) // 2
         if arr[mid] == target:
-            while arr[mid] == target:
-                mid += 1
-            return mid - 1
+            result = mid
+            low = mid + 1
         elif arr[mid] > target:
             high = mid - 1 
         else:
             low = mid + 1
-    return - 1
+    return result
 #print(binary_search_last([1, 3, 3, 4, 12, 12, 12, 14], 12))
+#print(binary_search_last([3,3,3],3))
 
 def binary_low_bound(arr, target, low=0, high=None):
     if high is None:
-        high = len(arr) - 1
+        high = len(arr)
     
-    while high >= low:
+    while high > low:
         mid = low + (high - low) // 2
 
-        if arr[mid] == target:
-            while arr[mid] == target:
-                mid -= 1
-            return mid + 1
-        if arr[mid] > target:
-            high = mid - 1
-        else:
+        if arr[mid] < target:
             low = mid + 1
-    return len(arr) if arr[0] < target else 0
-#print(binary_low_bound([3, 3, 4, 12, 12, 12, 14], 20))
+        else:
+            high = mid
+    return low
+print(binary_low_bound([3, 3, 4, 12, 12, 12, 14], 4))
+
+def binary_high_bound(arr, target, low=0, high=None):
+    if high is None:
+        high = len(arr)
+    
+    while high > low:
+        mid = low + (high - low) // 2
+
+        if arr[mid] <= target:
+            low = mid + 1
+        else:
+            high = mid
+    return high 
+print(binary_high_bound([3, 3, 4, 12, 12, 12, 14], 12))
--- alg_dat/exam_prep/binary_search/binary_search_timed.py
+++ alg_dat/exam_prep/binary_search/binary_search_timed.py
@@ -1,11 +1,32 @@
 # A1 timed
 def binary_search(arr, target, low=0, high=None):
-    pass
+    if high is None:
+        high = len(arr) - 1
 
-#print(binary_search_search([1, 3, 3, 4, 12, 12, 12, 14], 3))
+    while high >= low:
+        mid = low + (high - low) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+#print(binary_search([1, 3, 3, 4, 12, 12, 12, 14], 3))
 
 # B3 timed
 def binary_search_lower_bound(arr, target, low=0, high=None):
-    pass
+    if high is None:
+        high = len(arr)
 
-#print(binary_search_lower_bound([1, 3, 3, 4, 12, 12, 12, 14], 3))
+    while high > low:
+        mid = low + (high - low) // 2
+        if arr[mid] > target:
+            high = mid
+        else:
+            low = mid + 1
+
+    return low
+print(binary_search_lower_bound([1, 3, 3, 4, 12, 12, 12, 14], 15))
+print(binary_search_lower_bound([1, 5, 10], 7))
