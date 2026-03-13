91e2aa20bab04499944e17f8fe3b94a4e70e3319
lukantozi <lukamamri@outlook.com>
2026-03-13 15:31:03 +0100

    red rectangle moving and reacting to edges

---
+++ physics/window.c
@@ -0,0 +1,55 @@
+#include "raylib.h"
+#include "stdlib.h"
+#include "time.h"
+#include "math.h"
+
+#define WIDTH 800
+#define HEIGHT 600
+#define RECT_WIDTH 10
+#define RECT_HEIGHT 10
+
+typedef struct {
+    double pos_x;
+    double pos_y;
+    double direction;
+} rect;
+
+int main(void) {
+    srand(time(NULL));
+
+    InitWindow(WIDTH, HEIGHT, "raylib [core] example - basic window");
+    rect red_rect;
+    red_rect.pos_x = 100;
+    red_rect.pos_y = 100;
+    red_rect.direction = (rand() % 360) * (M_PI / 180);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(BLACK);
+
+            DrawRectangle(red_rect.pos_x, red_rect.pos_y, RECT_WIDTH, RECT_HEIGHT, RED);
+
+            red_rect.pos_x += cos(red_rect.direction) * 0.3;
+            red_rect.pos_y += sin(red_rect.direction) * 0.3;
+
+            if (red_rect.pos_x > WIDTH) {
+                red_rect.pos_x = WIDTH - (float)RECT_WIDTH / 2;
+                red_rect.direction = (rand() % 360) * (M_PI / 180);
+            } else if (red_rect.pos_x < 0) {
+                red_rect.pos_x = (float)RECT_WIDTH / 2;
+                red_rect.direction = (rand() % 360) * (M_PI / 180);
+            }
+            if (red_rect.pos_y > HEIGHT) {
+                red_rect.pos_y = HEIGHT - (float)RECT_HEIGHT / 2;
+                red_rect.direction = (rand() % 360) * (M_PI / 180);
+            } else if (red_rect.pos_y < 0) {
+                red_rect.pos_y = (float)RECT_HEIGHT;
+                red_rect.direction = (rand() % 360) * (M_PI / 180);
+            }
+        EndDrawing();
+    }
+    CloseWindow();
+
+    return 0;
+}
